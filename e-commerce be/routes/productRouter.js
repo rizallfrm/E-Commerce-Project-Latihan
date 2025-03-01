2d5e70bf194ba6ec3317751f17e67071e86e80bf
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-01 17:09:27 +0700

    modified: update productRouter

--- e-commerce be/routes/productRouter.js
+++ e-commerce be/routes/productRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductDetail, createProduct } = require('../controllers/product.controller');
+const { 
+  getProducts, 
+  getProductDetail, 
+  createProduct, 
+  updateProduct, 
+  deleteProduct,
+  addProductImage,
+  deleteProductImage 
+} = require('../controllers/productController');
 const { authenticateToken, isAdmin } = require('../middleware/auth');
 
 router.get('/', getProducts);
 router.get('/:id', getProductDetail);
 router.post('/', authenticateToken, isAdmin, createProduct);
+router.put('/:id', authenticateToken, isAdmin, updateProduct);
+router.delete('/:id', authenticateToken, isAdmin, deleteProduct);
+router.post('/:id/images', authenticateToken, isAdmin, addProductImage);
+router.delete('/:productId/images/:imageId', authenticateToken, isAdmin, deleteProductImage);
 
 module.exports = router;
