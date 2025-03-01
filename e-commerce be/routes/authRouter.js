efa8514347a58a1fcc0f79124efa858faae357dd
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-03-01 14:39:40 +0700

    update: add router in folder routes

--- e-commerce be/routes/authRouter.js
+++ e-commerce be/routes/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getProfile } = require('../controllers/auth.controller');
+const { register, login, getProfile } = require('../controllers/authController');
 const { authenticateToken } = require('../middleware/auth');
 
 router.post('/register', register);
